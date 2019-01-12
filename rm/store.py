5bfa491f595412aee0d74175dabd6831d0b898ab
Rex Low <qiweilow950823@gmail.com>
2019-01-12 21:30:22 +0800

    add store module

---
+++ rm/store.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+from rm import RMSDKModel
+from rm.utils import Request
+
+class Store(RMSDKModel):
+
+    def __init__(self, configs):
+        super(Store, self).__init__(configs)
+        self.defaults = {
+            "environment": configs["environment"],
+            "privateKey": configs["privateKey"],
+        }
+
+        for (param, default) in self.defaults.items():
+            setattr(self, param, configs.get(param, default))
+
+    def getStores(self, accessToken):
+        request = Request(environment=self.environment, endpoint="v3/stores")
+        headers, _ = self.getHeadersAndData(accessToken=accessToken, data=None, requestUrl=request.baseUrl, method="get")
+        response = request.doGet(headers=headers)
+        return response
+
+    def getStoreByID(self, accessToken, storeID):
+        request = Request(environment=self.environment, endpoint="v3/store/{}".format(storeID))
+        headers, _ = self.getHeadersAndData(accessToken=accessToken, data=None, requestUrl=request.baseUrl, method="get")
+        response = request.doGet(headers=headers)
+        return response
+
+    def createStore(self, accessToken, data):
+        request = Request(environment=self.environment, endpoint="v3/store")
+        headers, data = self.getHeadersAndData(accessToken=accessToken, data=data, requestUrl=request.baseUrl)
+        response = request.doPost(headers=headers, data=data)
+        return response
+
+    def updateStore(self, accessToken, storeID, data):
+        request = Request(environment=self.environment, endpoint="v3/store/{}".format(storeID))
+        headers, data = self.getHeadersAndData(accessToken=accessToken, data=data, requestUrl=request.baseUrl, method="patch")
+        response = request.doPatch(headers=headers, data=data)
+        return response
+
+    def deleteStore(self, accessToken, storeID):
+        request = Request(environment=self.environment, endpoint="v3/store/{}".format(storeID))
+        headers, _ = self.getHeadersAndData(accessToken=accessToken, data=None, requestUrl=request.baseUrl, method="delete")
+        response = request.doDelete(headers=headers)
+        return response
