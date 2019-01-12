ca0c0ff1e997b5a839d314531c921cc578dd17a1
Rex Low <qiweilow950823@gmail.com>
2019-01-12 21:30:46 +0800

    add merchant module

---
+++ rm/merchant.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from rm import RMSDKModel
+from rm.utils import Request
+
+class Merchant(RMSDKModel):
+
+    def __init__(self, configs):
+        super(Merchant, self).__init__(configs)
+        self.defaults = {
+            "environment": configs["environment"],
+            "privateKey": configs["privateKey"],
+        }
+
+        for (param, default) in self.defaults.items():
+            setattr(self, param, configs.get(param, default))
+    
+    def getMerchantProfile(self, accessToken):
+        request = Request(environment=self.environment, endpoint="v3/merchant")
+        headers, _ = self.getHeadersAndData(accessToken=accessToken, data=None, requestUrl=request.baseUrl, method="get")
+        response = request.doGet(headers=headers)
+        return response
+
+    def getMerchantSubcriptions(self, accessToken):
+        request = Request(environment=self.environment, endpoint="v3/merchant/subscriptions")
+        headers, _ = self.getHeadersAndData(accessToken=accessToken, data=None, requestUrl=request.baseUrl, method="get")
+        response = request.doGet(headers=headers)
+        return response
